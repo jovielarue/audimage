4762965c8294d3677bafd1a82fd6defc950bb836
jovielarue <44442031+jovielarue@users.noreply.github.com>
2024-12-17 13:48:13 -0700

    converting image to grayscale for less bit transfer

--- src/images/mod.rs
+++ src/images/mod.rs
@@ -1,49 +1,28 @@
-use image::{DynamicImage, GenericImageView, Rgba};
-use std::{ops::Index, path::Path};
+use image::{DynamicImage, GenericImageView, Luma};
+use std::path::Path;
 
-pub fn read_img_pixels(input_path: &str) -> Vec<(u32, u32, Rgba<u8>)> {
+pub fn read_img_info(input_path: &str) -> ((u32, u32), Vec<(u32, u32, Luma<u8>)>) {
     let img = image::open(&Path::new(input_path)).expect("Unable to open image.");
 
     // can possibly use img_dimensions for transmitting data x by x or y by y
-    let img = resize_img(&img);
-    let img_dimensions = img.dimensions();
-    println!(
-        "New image dimensions are {:?}x{:?}",
-        img_dimensions.0, img_dimensions.1
-    );
+    let resized_img = resize_img(&img);
+    let luma_img = resized_img.to_luma8();
+    let (width, height) = img.dimensions();
 
-    img.pixels().collect::<Vec<_>>()
+    let pixels: Vec<(u32, u32, Luma<u8>)> = luma_img
+        .enumerate_pixels()
+        .map(|(x, y, pixel)| (x, y, Luma::from(pixel.clone())))
+        .collect();
+
+    ((width, height), pixels)
 }
 
 pub fn resize_img(img: &DynamicImage) -> DynamicImage {
-    img.resize(300, 300, image::imageops::FilterType::Nearest)
+    img.resize(100, 100, image::imageops::FilterType::Nearest)
 }
 
-// maps a pixel rgb value (0-255) to a range between 400 and 3000
-// this ensures all pitches are within an audible range
-fn smooth_rgb_value(input: u8) -> f32 {
-    let input = input as f32;
-    let min_pitch: f32 = 400.0;
-    let max_pitch: f32 = 3000.0;
+pub fn process_rgb_vals(pixel: &(u32, u32, Luma<u8>)) {
+    let luma_val = pixel.2[0];
 
-    // get a value between 0 and 1
-    let normalized = f32::max(0.0, f32::min(1.0, input / 255.0));
-
-    // map the normalized value to the target range (between 400 and 3000)
-    let output = min_pitch + normalized * (max_pitch - min_pitch);
-
-    output
-}
-
-pub fn extract_rgb_nums_into_pitch(pixel: &Rgba<u8>) -> [f32; 3] {
-    let r = *pixel.index(0);
-    let g = *pixel.index(1);
-    let b = *pixel.index(2);
-
-    let pitch0 = smooth_rgb_value(r);
-    let pitch1 = smooth_rgb_value(g);
-    let pitch2 = smooth_rgb_value(b);
-    println!("{},{},{}", pitch0, pitch1, pitch2);
-
-    [pitch0, pitch1, pitch2]
+    println!("{:?}", luma_val);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
+mod dialup;
 mod images;
-mod pitches;
-use images::{extract_rgb_nums_into_pitch, read_img_pixels};
-use pitches::play_rgb_tone;
-use rodio::{OutputStream, Sink};
-use std::time::Duration;
+use images::{process_rgb_vals, read_img_info};
 
 fn main() {
-    // stream and sink are for outputting audio
-    let (_stream, stream_handle) =
-        OutputStream::try_default().expect("Unable to get default output stream.");
-    let sink = Sink::try_new(&stream_handle).expect("Unable to get new sink.");
+    let (dimensions, pixels) = read_img_info("/home/jovie/Pictures/seeingrainbows.jpeg");
+    println!("Image dimensions are {:?}x{:?}", dimensions.0, dimensions.1);
 
-    let pixels = read_img_pixels("/home/jovie/Pictures/seeingrainbows.jpeg");
     for pixel in pixels {
         // pixel.2 is the Rgba<u8> struct from Pixel
-        let pitches = extract_rgb_nums_into_pitch(&pixel.2);
-
-        let mut i = 1;
-        for pitch in pitches {
-            // every 3rd note (after r,g,b have been played) play a reference note
-            if i % 3 == 0 {
-                play_rgb_tone(350.0, Duration::from_millis(300), 0.2, &sink);
-            }
-
-            play_rgb_tone(pitch, Duration::from_millis(80), 0.1, &sink);
-            i += 1;
-        }
+        print!("{},{}:", pixel.0, pixel.1);
+        process_rgb_vals(&pixel);
     }
 }
