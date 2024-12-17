0e68818e138df2114f5e80f5f9dfb2b5c127c8b6
jovielarue <44442031+jovielarue@users.noreply.github.com>
2024-12-17 13:58:08 -0700

    get and print pixel map

--- src/images/mod.rs
+++ src/images/mod.rs
@@ -1,5 +1,5 @@
 use image::{DynamicImage, GenericImageView, Luma};
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
 pub fn read_img_info(input_path: &str) -> ((u32, u32), Vec<(u32, u32, Luma<u8>)>) {
     let img = image::open(&Path::new(input_path)).expect("Unable to open image.");
@@ -21,8 +21,17 @@ pub fn resize_img(img: &DynamicImage) -> DynamicImage {
     img.resize(100, 100, image::imageops::FilterType::Nearest)
 }
 
-pub fn process_rgb_vals(pixel: &(u32, u32, Luma<u8>)) {
+pub fn process_luma_val(
+    pixel: &(u32, u32, Luma<u8>),
+    pixel_map: &mut HashMap<u8, Vec<(u32, u32)>>,
+) {
     let luma_val = pixel.2[0];
+    let luma_coords = (pixel.0, pixel.1);
+
+    pixel_map
+        .entry(luma_val)
+        .and_modify(|coords| coords.push(luma_coords))
+        .or_insert(vec![luma_coords]);
 
     println!("{:?}", luma_val);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 mod dialup;
 mod images;
-use images::{process_rgb_vals, read_img_info};
+use std::collections::HashMap;
+
+use images::{process_luma_val, read_img_info};
 
 fn main() {
     let (dimensions, pixels) = read_img_info("/home/jovie/Pictures/seeingrainbows.jpeg");
     println!("Image dimensions are {:?}x{:?}", dimensions.0, dimensions.1);
 
+    // pixel_map stores the luma value with a vec of all coordinates where the luma value appears
+    let mut pixel_map: HashMap<u8, Vec<(u32, u32)>> = HashMap::new();
+
     for pixel in pixels {
         // pixel.2 is the Rgba<u8> struct from Pixel
         print!("{},{}:", pixel.0, pixel.1);
-        process_rgb_vals(&pixel);
+        process_luma_val(&pixel, &mut pixel_map);
     }
+
+    println!("Pixel map is: {:?}", pixel_map);
+    println!("Pixel map luma values are: {:?}", pixel_map.keys());
 }
